f54d5c5459534ef353ed5be596a794e897c2b5d3
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-22 11:43:14 -0300

    feat: service Buscar Produto Por Nome

--- smashpoint/src/main/java/br/com/lanchonete/smashpoint/service/produto/BuscarProdutoPorNome.java
+++ smashpoint/src/main/java/br/com/lanchonete/smashpoint/service/produto/BuscarProdutoPorNome.java
@@ -1,4 +1,35 @@
 package br.com.lanchonete.smashpoint.service.produto;
 
+import br.com.lanchonete.smashpoint.dto.responses.ProdutoResponseDto;
+import br.com.lanchonete.smashpoint.exception.ProdutoNaoEncontradoException;
+import br.com.lanchonete.smashpoint.model.Produto;
+import br.com.lanchonete.smashpoint.repository.ProdutoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BuscarProdutoPorNome {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoResponseDto buscar(String nome){
+
+        Produto produto = produtoRepository.findByNome(nome)
+                .orElseThrow(() -> new ProdutoNaoEncontradoException("Produto não encontrado"));
+
+        return gerarProdutoResponse(produto);
+    }
+
+    private ProdutoResponseDto gerarProdutoResponse(Produto p) {
+        return new ProdutoResponseDto(
+                p.getId(),
+                p.getNome(),
+                p.getDescricao(),
+                p.getCategoria().toString(),
+                p.getPreco().doubleValue(),
+                p.getStatus()
+        );
+    }
+
 }
