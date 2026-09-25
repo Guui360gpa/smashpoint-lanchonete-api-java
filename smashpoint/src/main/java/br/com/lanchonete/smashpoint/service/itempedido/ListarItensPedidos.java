61e845abfe317c24ebaa83a31fa78f7af2f3395b
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-25 17:19:35 -0300

    refactor: simplificando a service Listar Itens Pedidos

--- smashpoint/src/main/java/br/com/lanchonete/smashpoint/service/itempedido/ListarItensPedidos.java
+++ smashpoint/src/main/java/br/com/lanchonete/smashpoint/service/itempedido/ListarItensPedidos.java
@@ -23,25 +23,15 @@ public class ListarItensPedidos {
     private final PedidoRepository pedidoRepository;
 
     public List<ItemPedidoResponseDto> listar(Long idPedido){
-        Pedido pedido = pedidoRepository.findById(idPedido)
-                .orElseThrow(() -> new PedidoNaoEncontradoException("Pedido não encontrado"));
+        if (!pedidoRepository.existsById(idPedido)) {
+            throw new PedidoNaoEncontradoException("Pedido não encontrado");
+        }
 
         List<ItemPedido> itens = itemPedidoRepository.findByPedidoId(idPedido)
                 .orElseThrow(() ->new ListaItemPedidoVaziaException("Nenhum item pedido realizado"));
 
-        return gerarListaItemPedidoResponse(itens);
+        return itens.stream()
+                .map(ItemPedidoResponseDto::fromEntity)
+                .toList();
     }
-
-    public List<ItemPedidoResponseDto> gerarListaItemPedidoResponse(List<ItemPedido> itemPedidos){
-        return itemPedidos.stream()
-                .map(i -> new ItemPedidoResponseDto(
-                        i.getId(),
-                        i.getProduto().getNome(),
-                        i.getPedido().getNumeroMesa(),
-                        i.getQuantidade(),
-                        i.getPrecoUnitario().doubleValue(),
-                        i.getTotal().doubleValue()
-                )).toList();
-    }
-
 }
