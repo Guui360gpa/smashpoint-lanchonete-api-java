4f7cd9cc0abdcf54a8e409da8317d94c5e901503
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-26 00:03:57 -0300

    refactor: refatora classe Controller Cliente

--- smashpoint/src/main/java/br/com/lanchonete/smashpoint/controller/ClienteController.java
+++ smashpoint/src/main/java/br/com/lanchonete/smashpoint/controller/ClienteController.java
@@ -1,44 +1,45 @@
 package br.com.lanchonete.smashpoint.controller;
 
+import br.com.lanchonete.smashpoint.dto.requests.ClienteRequestDto;
+import br.com.lanchonete.smashpoint.dto.responses.ClienteResponseDto;
+import br.com.lanchonete.smashpoint.service.cliente.BuscarClientePorCpf;
+import br.com.lanchonete.smashpoint.service.cliente.CadastrarCliente;
+import br.com.lanchonete.smashpoint.service.cliente.ListarClientes;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Scanner;
 
-@Controller
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteController{
 
-    private Scanner read = new Scanner(System.in);
-    private int opcao;
+    private final CadastrarCliente cadastrarCliente;
+    private final BuscarClientePorCpf buscarClientePorCpf;
+    private final ListarClientes listarClientes;
 
+    @PostMapping
+    public ResponseEntity<ClienteResponseDto> cadastro(@Valid  @RequestBody ClienteRequestDto dto){
+        ClienteResponseDto clienteResponse = cadastrarCliente.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteResponse);
+    }
 
-    @Autowired
-    private ClienteService clienteService;
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDto>> lista(){
+        List<ClienteResponseDto> clienteResponses = listarClientes.listar();
+        return ResponseEntity.ok(clienteResponses);
+    }
 
-    public void exibirMenuCliente(){
-        while (true){
-
-            System.out.println("""
-                    
-                    [1] Cadastrar Cliente
-                    [2] Listar Clientes
-                    [3] Buscar Clientes
-                    """);
-            opcao = read.nextInt();
-
-            switch (opcao){
-                case 1:
-                    clienteService.cadastrar();
-                    break;
-                case 2:
-                    clienteService.listar();
-                    break;
-                case 3:
-                    clienteService.buscar();
-                    break;
-                default:
-                    break;
-            }
-        }
+    @GetMapping("/{cpf}")
+    public ResponseEntity<ClienteResponseDto> buscar(@PathVariable String cpf){
+        ClienteResponseDto clienteResponse = buscarClientePorCpf.buscar(cpf);
+        return ResponseEntity.ok(clienteResponse);
     }
 }
