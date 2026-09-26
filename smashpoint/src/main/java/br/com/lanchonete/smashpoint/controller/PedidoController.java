366f16f611c571d06d83cd7bbbd525754546bcc4
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-26 00:25:45 -0300

    refactor: refatora classe Controller Pedido

--- smashpoint/src/main/java/br/com/lanchonete/smashpoint/controller/PedidoController.java
+++ smashpoint/src/main/java/br/com/lanchonete/smashpoint/controller/PedidoController.java
@@ -1,18 +1,57 @@
 package br.com.lanchonete.smashpoint.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import br.com.lanchonete.smashpoint.dto.requests.PedidoRequestDto;
+import br.com.lanchonete.smashpoint.dto.responses.PedidoResponseDto;
+import br.com.lanchonete.smashpoint.service.pedido.*;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+@RequiredArgsConstructor
 public class PedidoController{
 
-    @Autowired
-    private PedidoService pedidoService;
+    private final NovoPedido novoPedido;
+    private final ListarPedidosAtivos listarPedidosAtivos;
+    private final ListarPedidosInativos listarPedidosInativos;
+    private final AtivarPedido ativarPedido;
+    private final DesativarPedido desativarPedido;
 
-    public void realizarPedido(){
-
-                pedidoService.realizar();
-
+    @PostMapping
+    public ResponseEntity<PedidoResponseDto> novo(@Valid @RequestBody PedidoRequestDto dto){
+        PedidoResponseDto pedidoResponse = novoPedido.novo(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoResponse);
     }
+
+    @GetMapping("/ativos")
+    public ResponseEntity<List<PedidoResponseDto>> listaAtivo(){
+        List<PedidoResponseDto> pedidoResponses = listarPedidosAtivos.listar();
+        return ResponseEntity.ok(pedidoResponses);
+    }
+
+    @GetMapping("/inativos")
+    public ResponseEntity<List<PedidoResponseDto>> listaInativos(){
+        List<PedidoResponseDto> pedidoResponses = listarPedidosInativos.listar();
+        return ResponseEntity.ok(pedidoResponses);
+    }
+
+    @PatchMapping("/{id}/desativar")
+    public ResponseEntity<PedidoResponseDto> desativa(@PathVariable Long id){
+        PedidoResponseDto pedidoResponse = desativarPedido.desativar(id);
+        return ResponseEntity.ok(pedidoResponse);
+    }
+
+    @PatchMapping("/{id}/ativar")
+    public ResponseEntity<PedidoResponseDto> ativa(@PathVariable Long id){
+        PedidoResponseDto pedidoResponse = ativarPedido.ativar(id);
+        return ResponseEntity.ok(pedidoResponse);
+    }
+
+
 }
